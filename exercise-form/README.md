655f5c9e80e234afda84ea0257572374afe97bb3
tomastrajan <tomas.trajan@gmail.com>
2024-02-24 20:39:51 +0100

    feat(form): update project baseline, add todo descriptions

--- exercise-form/README.md
+++ exercise-form/README.md
@@ -4,26 +4,17 @@
 
 by [@tomastrajan](https://twitter.com/tomastrajan) from [AngularExperts.io](https://angularexperts.io)
 
-# Welcome to exercise - Forms and validation
+# Welcome to exercise - Form 
 
-In this exercise, we're going to explore how to use Angular forms and validation to create a simple form with validation and error handling.
+In this exercise, we're going to explore how to define and display Angular forms using reactive forms approach
 
 - How to implement form using Angular Reactive Forms
 - How to implement nested from controls, form groups and form arrays
 - How to react to form value changes (eg list of options or dependent fields)
-- How to implement form validation using built-in and custom validators
-- How to display form validation errors on submit
-- How to reset the form to its initial state
-- How to integrate form with external data source (eg, API or store)
-- How to prevent users from navigating away from the form with unsaved changes
 
-> Compared to where we left off we have added a couple of new components and services to stramline
-> implementation of this exercise...
-
-- **product-editor-skeleton** component that displays skeleton UI version of editor while the item is being loaded,
-- **card-status** component to display statuses like error or success in a nicer way
-- **dialog-confirm** pattern which comes with a service which allows us to open a confirm dialog and get notified about the result of the user interaction.
-- **category** service which provides list of categories as a simple array of strings exposed as signal
+> Compared to where we left off we have added a
+> **category** service which provides list of categories as a
+> simple array of strings exposed as signal
 
 **Search for the  `TODO <index>:`, eg `TODO 1:`  items in the code itself by searching the `projects` folder using IDE or text editor search functionality**
 
--- exercise-form/projects/product-app/src/app/feature/product/product-editor/product-editor.component.ts
+++ exercise-form/projects/product-app/src/app/feature/product/product-editor/product-editor.component.ts
@@ -1,21 +1,104 @@
 import { RouterLink } from '@angular/router';
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+} from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { MatIconButton } from '@angular/material/button';
+import { MatButton, MatIconButton, MatMiniFabButton } from '@angular/material/button';
 
 import { CardComponent } from '../../../ui/card/card.component';
 import { buildMonthNamesAndShortYear } from '../../../core/util/date';
+import { FormArray, FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatError, MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CategoryService } from '../../../core/category/category.service';
+import { debounceTime } from 'rxjs';
+import { MatAutocomplete, MatAutocompleteTrigger, MatOption } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'my-org-product-editor',
   standalone: true,
-  imports: [RouterLink, MatIcon, MatIconButton, CardComponent, CardComponent],
+  imports: [
+    // TODO 1: import ReactiveFormsModule
+    RouterLink,
+    MatIcon,
+    MatIconButton,
+    CardComponent,
+    CardComponent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    FormsModule,
+    MatAutocomplete,
+    MatAutocompleteTrigger,
+    MatError,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatOption,
+    ReactiveFormsModule,
+    MatLabel,
+    MatMiniFabButton,
+    MatIcon,
+    MatIconButton,
+    MatSuffix,
+    MatButton,
+  ],
   templateUrl: './product-editor.component.html',
   styleUrl: './product-editor.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductEditorComponent {
+  // TODO 2: inject FormBuilder service
+
+  // TODO 11: inject CategoryService (form core)
+
   MONTHS = buildMonthNamesAndShortYear().reverse();
 
   productId = input<string>();
+
+  // TODO 3: crate a new form property and initialize it with the formBuilder.group method
+
+  // TODO 5: in the form group define basic fields for the product, use array shorthand syntax
+  // "name", "description" as strings, initialize them with empty string
+  // "price", "quantity" as numbers, initialize them as null (prefix with generic type <number|null>
+
+  // TODO 7: let's add a new form field called "supplier" and initialize it
+  // with the nested form group using the formBuilder.group method
+  // the nested form group will have two fields, "name" and "origin" as strings
+
+  // TODO 9: let's add an additional field called "category" and initialize it with an empty string
+
+  // TODO 10: let's define new signal called "categoryInputValue"
+  // and define it with help of toSignal method which is going to subscribe
+  // to the valueChanges of the category control of the form (try to use IDE code completion to get the correct reference)
+  // (the Angular Reactive Forms are strongly typed since Angular v14 so we can just use . access)
+  // let's use debounceTime(250) in the pipe to debounce the input value changes
+  // let's provide empty string as an initial value for the signal
+
+  // TODO 12: let's create a new computed signal called "filteredCategoryOptions"
+  // it will use "categories" signal from CategoryService and "categoryInputValue" signal
+  // and combine them to filter the categories based on the input value
+
+
+  // TODO 14:  now we're going to implement form array for the price per month field
+  // the form arrays have the most complex handling of all form control types
+  // first let's add "pricePerMonth" field to the form group
+  // and initialize it with the formBuilder.array method with empty array as the first argument
+
+  // TODO 15: let's define a getter for the pricePerMonth field which will get it
+  // with help of property access from the form controls and cast it to the FormArray type manually
+
+  // TODO 16: let's add a new method called "addPricePerMonth" which will accept an optional price parameter
+  // and will push a new FormControl to the pricePerMonth form array
+  // the form control will be initialized with price or 0 if the price is not provided
+
+  // TODO 17: let's add a new method called "removePricePerMonth" which will accept an index parameter
+  // and will remove the form control from the pricePerMonth form array at the given index
+  // using provided method of the FormArray type (try IDE code completion to get the correct reference)
 }
