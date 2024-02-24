3ae73c0b0d95a84e42ff755546f4ab9919188bda
tomastrajan <tomas.trajan@gmail.com>
2024-02-24 11:33:46 +0100

    feat(finished): unify animations params

--- exercise-finished/projects/order-app/src/app/ui/animation/appear-down.animation.ts
+++ exercise-finished/projects/order-app/src/app/ui/animation/appear-down.animation.ts
@@ -8,14 +8,14 @@ import {
 import { ANIMATION_TIME_AND_EASING } from './animation.const';
 
 export const appearDownAnimation = trigger('appearDown', [
-  state('false', style({ opacity: 0, transform: 'translateY(-20px)' })),
+  state('false', style({ opacity: 0, transform: 'translateY(-10px)' })),
   state('true', style({ opacity: 1, transform: 'translateY(0px)' })),
   transition('false <=> true', animate(ANIMATION_TIME_AND_EASING)),
 ]);
 
 export const appearDownEnterLeaveAnimation = trigger('appearDownEnterLeave', [
   transition(':enter', [
-    style({ opacity: 0, transform: 'translateY(-15px)' }),
+    style({ opacity: 0, transform: 'translateY(-10  px)' }),
     animate(
       ANIMATION_TIME_AND_EASING,
       style({ opacity: 1, transform: 'translateY(0px)' }),
@@ -24,7 +24,7 @@ export const appearDownEnterLeaveAnimation = trigger('appearDownEnterLeave', [
   transition(':leave', [
     animate(
       ANIMATION_TIME_AND_EASING,
-      style({ opacity: 0, transform: 'translateY(-15px)' }),
+      style({ opacity: 0, transform: 'translateY(-10px)' }),
     ),
   ]),
 ]);
