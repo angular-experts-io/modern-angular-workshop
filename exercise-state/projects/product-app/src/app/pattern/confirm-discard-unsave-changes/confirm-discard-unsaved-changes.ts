9ab281a61bcefa04e72ea702de8e511cd916af02
tomastrajan <tomas.trajan@gmail.com>
2024-02-25 20:44:48 +0100

    feat(state): exercise baseline impl fix

--- exercise-state/projects/product-app/src/app/pattern/confirm-discard-unsave-changes/confirm-discard-unsaved-changes.ts
+++ exercise-state/projects/product-app/src/app/pattern/confirm-discard-unsave-changes/confirm-discard-unsaved-changes.ts
@@ -11,13 +11,6 @@ export function confirmDiscardUnsavedChanges(form: AbstractControl) {
       isInfo: true,
     });
   }
-  return false;
+  return true;
 }
 
-// let's create a"confirmDiscardUnsavedChanges" function which will accept a form (with AbstractControl type)
-// inside the function were going to check it the provided form is dirty
-// if it is dirty we're going to inject DialogConfirmService and use its open$ method to open a dialog
-// (the inline injection works because guards run in the injection context)
-// the call will be parametrized with title, message, and isInfo flag
-// we're going to return the result of the open$ method
-// if the form is not dirty we're going to return true
