9673a8a31cd0f0960c9a6b6e0bc28ccccb5814e0
tomastrajan <tomas.trajan@gmail.com>
2024-12-03 23:24:50 +0100

    feat(routing): improve task descriptions

--- exercise-routing/projects/product-app/src/app/feature/product/product-list/product-list.component.ts
+++ exercise-routing/projects/product-app/src/app/feature/product/product-list/product-list.component.ts
@@ -44,8 +44,8 @@ import { ProductApiService } from '../product-api.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductListComponent {
-  private productApiService = inject(ProductApiService);
-  private refreshTrigger = new Subject<string>();
+  #productApiService = inject(ProductApiService);
+  #refreshTrigger = new Subject<string>();
 
   showFilter = signal(false);
   query = signal<string>('');
@@ -56,7 +56,7 @@ export class ProductListComponent {
 
   products = toSignal(
     toObservable(this.query).pipe(
-      mergeWith(this.refreshTrigger),
+      mergeWith(this.#refreshTrigger),
       debounceTime(300),
       tap(() => {
         if (this.products()?.length) {
@@ -67,7 +67,7 @@ export class ProductListComponent {
         this.error.set(undefined);
       }),
       switchMap((query) =>
-        this.productApiService.find(query).pipe(
+        this.#productApiService.find(query).pipe(
           catchError((error) => {
             this.error.set(error?.message?.toString());
             return [[]];
@@ -85,14 +85,15 @@ export class ProductListComponent {
   // TODO 13: reflecting UI state to URL query params
   // let's synchronize state of query signal to the URL to provide even better deep linking capabilities
   // first, let's inject the Router injectable
-  // lets define a constructor which will allow us to use Angular Signals "effect"
+  //
+  // let's define a constructor which will allow us to use Angular Signals "effect"
   // in the effect we will use router.navigate() method and navigate to the same route []
   // and then, in the second options object argument, we're going to specify queryParams
   // with query assigned to this.query() signal value, make sure to assign undefined if the query is empty
   // next we're going to specify queryParamsHandling to "merge" to not lose other query params
   // lastly, we want to set showFilter signal to true if the query signal has a value
   // let's try it in the running app, the URL should now reflect the query signal value
-
+  //
   // TODO 14: reflecting URL query param state to UI state
   // now we need to define a new signal input "queryFromUrl"
   // which will receive value from URL query params because we are using "withComponentInputBinding"
@@ -101,21 +102,21 @@ export class ProductListComponent {
   // an option object as the second argument, in the options object we're going to provide
   // "alias" property with the "query" as the value, that way we're going to automatically
   // receive correct value from the URL and can use it in our logic
-
+  //
   // after we're going to define a NEW effect which will be triggered when "queryFromUrl" signal changes
   // in the effect we're going to check if the "queryFromUrl" has a value
   // if it does, we're going to set the "query" signal value to the value from the "queryFromUrl" input
   // lastly, we're going to set the "showFilter" signal value to true if the query has a value
   // the effect is setting a signal value, so we will need to provide additional configuration, error will provide a hint
   // does the order of effects matter? why? (try adding log statements to the effects and see it in the console)
-
+  //
   // summary: it's much better and cleaner to handle this with NgRx and router-store, but it's a good exercise to understand the concept
 
   removeProduct(productId: string) {
     this.loading.set(true);
-    this.productApiService.remove(productId).subscribe({
+    this.#productApiService.remove(productId).subscribe({
       next: () => {
-        this.refreshTrigger.next(this.query());
+        this.#refreshTrigger.next(this.query());
         this.loading.set(false);
       },
       error: (error) => this.error.set(error?.message?.toString()),
