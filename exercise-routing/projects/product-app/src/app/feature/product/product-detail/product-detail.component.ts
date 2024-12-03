5b7c234d8e9f2b74ac3af9ae0f7a0b4cfe42c4ba
tomastrajan <tomas.trajan@gmail.com>
2024-12-03 23:13:10 +0100

    feat(routing): improve task descriptions

--- exercise-routing/projects/product-app/src/app/feature/product/product-detail/product-detail.component.ts
+++ exercise-routing/projects/product-app/src/app/feature/product/product-detail/product-detail.component.ts
@@ -12,13 +12,18 @@ export class ProductDetailComponent {
   // make it required as well and print it's value in the bottom of the template
   // check out the running app and see if the productId is printed correctly
   // what would happen if we renamed it from productId to id and why?
+  //
   // TODO 12: let's implement loading, error and product signals
   // the use-case is to load individual product from the API and display it
   // in the template, try to apply what we learned from the product list to make
-  // it happen using the same component state management approach
+  // we're going to use the same component based state management approach
+  //
   // the backend API supports "products/:id" endpoint
   // the product will be derived from the productId signal and will use RxJs interop
   // the loading can show existing  "product-item-skeleton" component
   // the product can be displayed by copying parts of the template form "product-item" component
   // you can reimplement the averagePrice computed signal as well (isolation)
+  //
+  // once done, comment the current solution
+  // and try to use the "rxResource" API and compare the implementation
 }
