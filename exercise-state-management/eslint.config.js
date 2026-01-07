a1d4080952330869207329156b0733a0e9b90523
tomastrajan <tomas.trajan@gmail.com>
2026-01-07 16:22:58 +0100

    feat(all): eslint 9 config files

---
+++ exercise-state-management/eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const { defineConfig } = require('eslint/config');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'my-org',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'my-org',
+          style: 'kebab-case',
+        },
+      ],
+      'no-unused-private-class-members': ['off'],
+      '@angular-eslint/no-output-native': ['off'],
+      '@angular-eslint/no-input-rename': ['off'],
+      '@angular-eslint/no-host-metadata-property': ['off'],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    rules: {
+      '@angular-eslint/template/button-has-type': 'error',
+    },
+  },
+]);
