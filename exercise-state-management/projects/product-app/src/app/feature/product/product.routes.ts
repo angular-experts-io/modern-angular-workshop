313dfaf169e6a0ee3ffa2186735b83ff80a43ba3
tomastrajan <tomas.trajan@gmail.com>
2026-01-14 21:33:48 +0100

    feat(state-management): baseline

--- exercise-state-management/projects/product-app/src/app/feature/product/product.routes.ts
+++ exercise-state-management/projects/product-app/src/app/feature/product/product.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { AbstractControl } from '@angular/forms';
+import { FieldTree } from '@angular/forms/signals';
 
 import { confirmDiscardUnsavedChanges } from '../../pattern/confirm-discard-unsave-changes/confirm-discard-unsaved-changes';
 
@@ -22,7 +22,7 @@ export default <Routes>[
                 (c) => c.ProductEditorComponent,
               ),
             canDeactivate: [
-              (component: { form: AbstractControl }) =>
+              (component: { form: FieldTree<unknown> }) =>
                 confirmDiscardUnsavedChanges(component.form),
             ],
           },
@@ -33,7 +33,7 @@ export default <Routes>[
                 (c) => c.ProductEditorComponent,
               ),
             canDeactivate: [
-              (component: { form: AbstractControl }) =>
+              (component: { form: FieldTree<unknown> }) =>
                 confirmDiscardUnsavedChanges(component.form),
             ],
           },
--- exercise-state-management/projects/product-app/src/app/pattern/confirm-discard-unsave-changes/confirm-discard-unsaved-changes.ts
+++ exercise-state-management/projects/product-app/src/app/pattern/confirm-discard-unsave-changes/confirm-discard-unsaved-changes.ts
@@ -1,10 +1,10 @@
-import { AbstractControl } from '@angular/forms';
 import { inject } from '@angular/core';
+import { FieldTree } from '@angular/forms/signals';
 
 import { DialogConfirmService } from '../dialog-confirm/dialog-confirm.service';
 
-export function confirmDiscardUnsavedChanges(form: AbstractControl) {
-  if (form.dirty) {
+export function confirmDiscardUnsavedChanges(form: FieldTree<unknown>) {
+  if (form().dirty()) {
     return inject(DialogConfirmService).open$({
       title: 'Discard unsaved changes?',
       message: 'Are you sure you want to discard unsaved changes?',
