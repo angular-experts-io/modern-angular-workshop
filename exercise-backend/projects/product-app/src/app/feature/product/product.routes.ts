bf6f1b315417ee1f19eae4bff8a6220461c9516d
tomastrajan <tomas.trajan@gmail.com>
2026-03-17 14:25:26 +0700

    fix(backend): baseline

--- exercise-backend/projects/product-app/src/app/feature/product/product.routes.ts
+++ exercise-backend/projects/product-app/src/app/feature/product/product.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { ProductListComponent } from './product-list/product-list.component';
-import { ProductApiService } from './product-api.service';
+
 
 export default <Routes>[
   {
     path: '',
-    providers: [ProductApiService],
+    providers: [],
     children: [
       {
         path: '',
