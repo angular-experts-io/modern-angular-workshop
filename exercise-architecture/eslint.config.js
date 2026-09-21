5e776e803e272a0d84671811a4ba110a08050cf5
Tomas Trajan <tomas.trajan@gmail.com>
2026-09-21 15:45:10 +0200

    fix(architecture): remove config that is added becasue of todo items

--- exercise-architecture/eslint.config.js
+++ exercise-architecture/eslint.config.js
@@ -4,10 +4,8 @@ const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 
-const boundaries = require('./eslint.config.boundaries.js');
 
 module.exports = defineConfig([
-  boundaries,
   {
     files: ['**/*.ts'],
     extends: [
